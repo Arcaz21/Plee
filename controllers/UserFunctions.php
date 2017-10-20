d42694a83462f7d3071e880fb94a41fc36606518
John Suarez <arcaz.johlns@gmail.com>
2017-10-20 12:52:22 +0800

    created function to for getting data from form

---
+++ controllers/UserFunctions.php
@@ -0,0 +1,18 @@
+<?php include '../models/UserModels.php'; ?>
+<?php
+	$db = new UserModels();
+
+	$add = "no";
+	$add = isset ($_REQUEST['add']) ? $_REQUEST['add']:"no";
+	
+	$data['fname'] = isset($_REQUEST['fname'])?$_REQUEST['fname']:NULL;
+	$data['email'] = isset($_REQUEST['email'])?$_REQUEST['email']:NULL;
+	$data['phone'] = isset($_REQUEST['phone'])?$_REQUEST['phone']:NULL;
+	$data['pass'] = isset($_REQUEST['pass'])?$_REQUEST['pass']:NULL;
+
+	if(isset($_REQUEST['submit']) && $_REQUEST['submit'] == "submit"){
+		$db->addUser($data);
+	}
+
+	print_r($data);
+?>
